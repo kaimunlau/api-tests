334d9b833875cb48510c08f119ae19b76ef37713
David Lau <david.kaimun.lau@gmail.com>
2022-02-14 17:46:12 +0100

    random chuck norris joke on load

--- src/ChuckNorris.js
+++ src/ChuckNorris.js
@@ -1,7 +1,22 @@
-const ChuckNorris = () => {
-    return(
-        <p>A Chuck Norris joke</p>
-    )
+import { Component } from "react"
+
+class ChuckNorris extends Component {
+    constructor() {
+        super()
+        this.state= {
+            joke: ''
+        }
+    }
+    componentDidMount() {
+        fetch('https://api.chucknorris.io/jokes/random')
+        .then(resp => resp.json())
+        .then(json => this.setState({ joke: json.value}))
+    }
+    render() {
+        return(
+            <p>{this.state.joke}</p>
+        )
+    }
 }
 
 export default ChuckNorris;
